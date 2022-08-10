de978f667e9c8c9583bd4d3f07b47d03aaee21ba
Patrick Purcell <patrick.purcell@intel.com>
2022-08-10 09:17:18 -0700

    setup-triggered-builds (#7)
    Sets up gvk triggered builds

--- cmake/glslang.cmake
+++ cmake/glslang.cmake
@@ -1,7 +1,8 @@
 
-set(ENABLE_GLSLANG_INSTALL OFF CACHE BOOL "" FORCE)
-set(ENABLE_GLSLANG_JS      OFF CACHE BOOL "" FORCE)
-set(ENABLE_GLSLANG_WEBMIN  OFF CACHE BOOL "" FORCE)
+set(ENABLE_GLSLANG_BINARIES OFF CACHE BOOL "" FORCE)
+set(ENABLE_GLSLANG_INSTALL  OFF CACHE BOOL "" FORCE)
+set(ENABLE_GLSLANG_JS       OFF CACHE BOOL "" FORCE)
+set(ENABLE_GLSLANG_WEBMIN   OFF CACHE BOOL "" FORCE)
 FetchContent_Declare(
     glslang
     GIT_REPOSITORY "https://github.com/KhronosGroup/glslang.git"
@@ -11,24 +12,20 @@ FetchContent_Declare(
 )
 FetchContent_MakeAvailable(glslang)
 set(folder "gvk/external/glslang/")
-set_target_properties(GenericCodeGen                  PROPERTIES FOLDER "${folder}")
-set_target_properties(glslang                         PROPERTIES FOLDER "${folder}")
-set_target_properties(glslang-default-resource-limits PROPERTIES FOLDER "${folder}")
-set_target_properties(MachineIndependent              PROPERTIES FOLDER "${folder}")
-set_target_properties(OGLCompiler                     PROPERTIES FOLDER "${folder}")
-set_target_properties(OSDependent                     PROPERTIES FOLDER "${folder}")
-set_target_properties(SPIRV                           PROPERTIES FOLDER "${folder}")
-set_target_properties(SPVRemapper                     PROPERTIES FOLDER "${folder}")
-set_target_properties(HLSL                            PROPERTIES FOLDER "${folder}/hlsl/")
-set_target_properties(glslangValidator                PROPERTIES FOLDER "${folder}/tools/")
-set_target_properties(spirv-remap                     PROPERTIES FOLDER "${folder}/tools/")
+set_target_properties(GenericCodeGen     PROPERTIES FOLDER "${folder}")
+set_target_properties(glslang            PROPERTIES FOLDER "${folder}")
+set_target_properties(MachineIndependent PROPERTIES FOLDER "${folder}")
+set_target_properties(OGLCompiler        PROPERTIES FOLDER "${folder}")
+set_target_properties(OSDependent        PROPERTIES FOLDER "${folder}")
+set_target_properties(SPIRV              PROPERTIES FOLDER "${folder}")
+set_target_properties(SPVRemapper        PROPERTIES FOLDER "${folder}")
+set_target_properties(HLSL               PROPERTIES FOLDER "${folder}/hlsl/")
 add_library(glslang_INTERFACE INTERFACE)
 target_link_libraries(
     glslang_INTERFACE
     INTERFACE
         GenericCodeGen
         glslang
-        glslang-default-resource-limits
         MachineIndependent
         OGLCompiler
         OSDependent
--- cmake/tinyxml2.cmake
+++ cmake/tinyxml2.cmake
@@ -1,6 +1,5 @@
 
-set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
-set(BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(tinyxml2_BUILD_TESTING OFF CACHE BOOL "" FORCE)
 FetchContent_Declare(
     tinyxml2 GIT_REPOSITORY
     "https://github.com/leethomason/tinyxml2.git"
@@ -9,6 +8,4 @@ FetchContent_Declare(
     FETCHCONTENT_UPDATES_DISCONNECTED
 )
 FetchContent_MakeAvailable(tinyxml2)
-set(folder "gvk/external/tinyxml2/")
-set_target_properties(tinyxml2 PROPERTIES FOLDER "${folder}")
-set_target_properties(xmltest PROPERTIES FOLDER "${folder}")
+set_target_properties(tinyxml2 PROPERTIES FOLDER "gvk/external/")
