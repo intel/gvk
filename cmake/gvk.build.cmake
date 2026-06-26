99974bd0418a6d0dd9bdff44b1fc35c7ba2618cf
Patrick Purcell <patrick.purcell@intel.com>
2026-06-26 13:18:30 -0700

    Debugging includes

--- cmake/gvk.build.cmake
+++ cmake/gvk.build.cmake
@@ -46,92 +46,37 @@ function(gvk_get_directory_targets directory outTargets)
     set(_gvkDirectoryTargets "" CACHE INTERNAL "")
 endfunction()
 
-#### function(gvk_setup_target)
-####     cmake_parse_arguments(ARGS "" "TARGET;FOLDER" "LINK_LIBRARIES;INCLUDE_DIRECTORIES;INCLUDE_FILES;SOURCE_FILES;COMPILE_DEFINITIONS" ${ARGN})
-####     string(FIND ${CMAKE_CURRENT_SOURCE_DIR} "${CMAKE_SOURCE_DIR}/internal/" gvkInternal)
-#### 
-####     if(gvkInternal GREATER_EQUAL 0)
-####         target_include_directories(${ARGS_TARGET} PUBLIC "$<BUILD_INTERFACE:${ARGS_INCLUDE_DIRECTORIES}>" INTERFACE "$<INSTALL_INTERFACE:internal/include>")
-####     else()
-####         target_include_directories(${ARGS_TARGET} PUBLIC "$<BUILD_INTERFACE:${ARGS_INCLUDE_DIRECTORIES}>" INTERFACE "$<INSTALL_INTERFACE:include>")
-####     endif()
-#### 
-####     get_target_property(targetType ${ARGS_TARGET} TYPE)
-####     if(targetType STREQUAL "EXECUTABLE")
-####         target_link_libraries(${ARGS_TARGET} PRIVATE ${ARGS_LINK_LIBRARIES})
-####     else()
-####         target_link_libraries(${ARGS_TARGET} PUBLIC ${ARGS_LINK_LIBRARIES})
-####     endif()
-#### 
-####     target_compile_definitions(${ARGS_TARGET} PUBLIC "${ARGS_COMPILE_DEFINITIONS}")
-####     # target_link_libraries(${ARGS_TARGET} PUBLIC ${ARGS_LINK_LIBRARIES})
-####     set_target_properties(${ARGS_TARGET} PROPERTIES LINKER_LANGUAGE CXX)
-####     target_compile_options(${ARGS_TARGET} PRIVATE $<$<CXX_COMPILER_ID:MSVC>:/W4 /WX> $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wall -Wextra -Wpedantic -Werror -fPIC>)
-####     gvk_create_file_group("${ARGS_INCLUDE_FILES}")
-####     gvk_create_file_group("${ARGS_SOURCE_FILES}")
-####     set_target_properties(${ARGS_TARGET} PROPERTIES FOLDER "${GVK_IDE_FOLDER}/${ARGS_FOLDER}")
-####     if(gvkInternal GREATER_EQUAL 0)
-####         set_target_properties(${ARGS_TARGET} PROPERTIES GVK_INTERNAL TRUE)
-####     endif()
-#### 
-####     # NOTE : Disabling constexpr std::mutex ctor to account for mismatch between
-####     #   the MSVC standard library headers and runtime.  It's not exactly ideal, but
-####     #   it is the most practical solution for the time being since this project is
-####     #   distributed in source code form.
-####     # FROM : https://github.com/microsoft/STL/releases/tag/vs-2022-17.10
-####     #   Fixed mutex's constructor to be constexpr.
-####     #   Note: Programs that aren't following the documented restrictions on binary compatibility may encounter null dereferences in mutex machinery. You must follow this rule:
-####     #   When you mix binaries built by different supported versions of the toolset, the Redistributable version must be at least as new as the latest toolset used by any app component.
-####     #   You can define _DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR as an escape hatch.
-####     # TODO : As MSVC/Visual Studio receive updates, pay attention to when would be
-####     #   a good time to remove this...presumably it will be deprecated at some point
-####     #   anyway.
-####     if (MSVC_VERSION GREATER_EQUAL 1930)
-####         target_compile_options(${ARGS_TARGET} PRIVATE -D_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR)
-####     endif()
-#### endfunction()
-
 function(gvk_setup_target)
     cmake_parse_arguments(ARGS "" "TARGET;FOLDER" "LINK_LIBRARIES;INCLUDE_DIRECTORIES;INCLUDE_FILES;SOURCE_FILES;COMPILE_DEFINITIONS" ${ARGN})
     string(FIND ${CMAKE_CURRENT_SOURCE_DIR} "${CMAKE_SOURCE_DIR}/internal/" gvkInternal)
-
-    # 1. Query the target type natively
-    get_target_property(targetType ${ARGS_TARGET} TYPE)
-
-    # 2. Dynamic Scope Assignment
-    if(targetType STREQUAL "EXECUTABLE")
-        # Executables have no downstream consumers; they use PRIVATE for their own build directories
-        if(ARGS_INCLUDE_DIRECTORIES)
-            target_include_directories(${ARGS_TARGET} PRIVATE "${ARGS_INCLUDE_DIRECTORIES}")
-        endif()
+    if(gvkInternal GREATER_EQUAL 0)
+        target_include_directories(${ARGS_TARGET} PUBLIC "$<BUILD_INTERFACE:${ARGS_INCLUDE_DIRECTORIES}>" INTERFACE $<INSTALL_INTERFACE:internal/include>)
     else()
-        # Libraries MUST use PUBLIC so downstream consumers can inherit their headers!
-        if(ARGS_INCLUDE_DIRECTORIES)
-            target_include_directories(${ARGS_TARGET} PUBLIC "$<BUILD_INTERFACE:${ARGS_INCLUDE_DIRECTORIES}>")
-        endif()
-
-        # Handle your package installation export boundaries for libraries only
-        if(gvkInternal GREATER_EQUAL 0)
-            target_include_directories(${ARGS_TARGET} INTERFACE "$<INSTALL_INTERFACE:internal/include>")
-        else()
-            target_include_directories(${ARGS_TARGET} INTERFACE "$<INSTALL_INTERFACE:include>")
-        endif()
+        target_include_directories(${ARGS_TARGET} PUBLIC "$<BUILD_INTERFACE:${ARGS_INCLUDE_DIRECTORIES}>" INTERFACE $<INSTALL_INTERFACE:include>)
     endif()
-
-    target_compile_definitions(${ARGS_TARGET} PUBLIC "${ARGS_COMPILE_DEFINITIONS}")
-    
-    target_link_libraries(${ARGS_TARGET} ${ARGS_LINK_LIBRARIES})
-
+    target_compile_definitions(${ARGS_TARGET} PUBLIC ${ARGS_COMPILE_DEFINITIONS})
+    target_link_libraries(${ARGS_TARGET} PUBLIC ${ARGS_LINK_LIBRARIES})
     set_target_properties(${ARGS_TARGET} PROPERTIES LINKER_LANGUAGE CXX)
     target_compile_options(${ARGS_TARGET} PRIVATE $<$<CXX_COMPILER_ID:MSVC>:/W4 /WX> $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wall -Wextra -Wpedantic -Werror -fPIC>)
     gvk_create_file_group("${ARGS_INCLUDE_FILES}")
     gvk_create_file_group("${ARGS_SOURCE_FILES}")
-    set_target_properties(${ARGS_TARGET} PROPERTIES FOLDER "${GVK_IDE_FOLDER}/${ARGS_FOLDER}")
-    
+    set_target_properties(${ARGS_TARGET} PROPERTIES FOLDER ${GVK_IDE_FOLDER}/${ARGS_FOLDER})
     if(gvkInternal GREATER_EQUAL 0)
         set_target_properties(${ARGS_TARGET} PROPERTIES GVK_INTERNAL TRUE)
     endif()
 
+    # NOTE : Disabling constexpr std::mutex ctor to account for mismatch between
+    #   the MSVC standard library headers and runtime.  It's not exactly ideal, but
+    #   it is the most practical solution for the time being since this project is
+    #   distributed in source code form.
+    # FROM : https://github.com/microsoft/STL/releases/tag/vs-2022-17.10
+    #   Fixed mutex's constructor to be constexpr.
+    #   Note: Programs that aren't following the documented restrictions on binary compatibility may encounter null dereferences in mutex machinery. You must follow this rule:
+    #   When you mix binaries built by different supported versions of the toolset, the Redistributable version must be at least as new as the latest toolset used by any app component.
+    #   You can define _DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR as an escape hatch.
+    # TODO : As MSVC/Visual Studio receive updates, pay attention to when would be
+    #   a good time to remove this...presumably it will be deprecated at some point
+    #   anyway.
     if (MSVC_VERSION GREATER_EQUAL 1930)
         target_compile_options(${ARGS_TARGET} PRIVATE -D_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR)
     endif()
@@ -141,13 +86,13 @@ function(gvk_add_static_library)
     cmake_parse_arguments(ARGS "" "TARGET;FOLDER" "LINK_LIBRARIES;INCLUDE_DIRECTORIES;INCLUDE_FILES;SOURCE_FILES;COMPILE_DEFINITIONS" ${ARGN})
     add_library(${ARGS_TARGET} STATIC "${ARGS_INCLUDE_FILES}" "${ARGS_SOURCE_FILES}")
     gvk_setup_target(
-     TARGET               ${ARGS_TARGET}
-     FOLDER              "${ARGS_FOLDER}"
-     LINK_LIBRARIES       ${ARGS_LINK_LIBRARIES}
-     INCLUDE_DIRECTORIES  ${ARGS_INCLUDE_DIRECTORIES}
-     INCLUDE_FILES       "${ARGS_INCLUDE_FILES}"
-     SOURCE_FILES        "${ARGS_SOURCE_FILES}"
-     COMPILE_DEFINITIONS  ${ARGS_COMPILE_DEFINITIONS}
+        TARGET              ${ARGS_TARGET}
+        FOLDER              ${ARGS_FOLDER}
+        LINK_LIBRARIES      ${ARGS_LINK_LIBRARIES}
+        INCLUDE_DIRECTORIES ${ARGS_INCLUDE_DIRECTORIES}
+        INCLUDE_FILES       ${ARGS_INCLUDE_FILES}
+        SOURCE_FILES        ${ARGS_SOURCE_FILES}
+        COMPILE_DEFINITIONS ${ARGS_COMPILE_DEFINITIONS}
     )
 endfunction()
 
@@ -155,13 +100,13 @@ function(gvk_add_executable)
     cmake_parse_arguments(ARGS "" "TARGET;FOLDER" "LINK_LIBRARIES;INCLUDE_DIRECTORIES;INCLUDE_FILES;SOURCE_FILES;COMPILE_DEFINITIONS" ${ARGN})
     add_executable(${ARGS_TARGET} "${ARGS_INCLUDE_FILES}" "${ARGS_SOURCE_FILES}")
     gvk_setup_target(
-        TARGET               ${ARGS_TARGET}
-        FOLDER              "${ARGS_FOLDER}"
-        LINK_LIBRARIES       ${ARGS_LINK_LIBRARIES}
-        INCLUDE_DIRECTORIES  ${ARGS_INCLUDE_DIRECTORIES}
-        INCLUDE_FILES       "${ARGS_INCLUDE_FILES}"
-        SOURCE_FILES        "${ARGS_SOURCE_FILES}"
-        COMPILE_DEFINITIONS  ${ARGS_COMPILE_DEFINITIONS}
+        TARGET              ${ARGS_TARGET}
+        FOLDER              ${ARGS_FOLDER}
+        LINK_LIBRARIES      ${ARGS_LINK_LIBRARIES}
+        INCLUDE_DIRECTORIES ${ARGS_INCLUDE_DIRECTORIES}
+        INCLUDE_FILES       ${ARGS_INCLUDE_FILES}
+        SOURCE_FILES        ${ARGS_SOURCE_FILES}
+        COMPILE_DEFINITIONS ${ARGS_COMPILE_DEFINITIONS}
     )
     set_target_properties(${ARGS_TARGET} PROPERTIES GVK_EXECUTABLE TRUE)
 endfunction()
@@ -169,17 +114,17 @@ endfunction()
 function(gvk_add_code_generator)
     cmake_parse_arguments(ARGS "" "TARGET;FOLDER" "LINK_LIBRARIES;INCLUDE_DIRECTORIES;INCLUDE_FILES;SOURCE_FILES;INPUT_FILES;OUTPUT_FILES;COMPILE_DEFINITIONS" ${ARGN})
     gvk_add_executable(
-        TARGET               ${ARGS_TARGET}
-        FOLDER              "${ARGS_FOLDER}"
-        LINK_LIBRARIES       ${ARGS_LINK_LIBRARIES}
-        INCLUDE_DIRECTORIES  ${ARGS_INCLUDE_DIRECTORIES}
-        INCLUDE_FILES       "${ARGS_INCLUDE_FILES}"
-        SOURCE_FILES        "${ARGS_SOURCE_FILES}"
-        COMPILE_DEFINITIONS  ${ARGS_COMPILE_DEFINITIONS}
+        TARGET              ${ARGS_TARGET}
+        FOLDER              ${ARGS_FOLDER}
+        LINK_LIBRARIES      ${ARGS_LINK_LIBRARIES}
+        INCLUDE_DIRECTORIES ${ARGS_INCLUDE_DIRECTORIES}
+        INCLUDE_FILES       ${ARGS_INCLUDE_FILES}
+        SOURCE_FILES        ${ARGS_SOURCE_FILES}
+        COMPILE_DEFINITIONS ${ARGS_COMPILE_DEFINITIONS}
     )
     add_custom_command(
         OUTPUT ${ARGS_OUTPUT_FILES}
-        COMMAND "${ARGS_TARGET}" "${ARGS_INPUT_FILES}"
+        COMMAND ${ARGS_TARGET} ${ARGS_INPUT_FILES}
         DEPENDS ${ARGS_TARGET} ${ARGS_INPUT_FILES} ${ARGS_LINK_LIBRARIES}
     )
 endfunction()
